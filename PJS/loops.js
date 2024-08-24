9f08eb026cccf8ac8ed5ecd14b85000ba0041669
Samra44 <154986251+Samra44@users.noreply.github.com>
2024-08-24 10:55:56 +0500

    Add files via upload
    PJS

---
+++ PJS/loops.js
@@ -0,0 +1,20 @@
+// let table =5;
+// for(let i =1;i<=10;i++){
+//     console.log(table + "X" +i+ "=" +(table*i));
+// }
+
+// object
+
+ student_info = {
+    name: "samra",
+    age:    "20",
+    skill:  "web developer",
+    }
+
+// for in
+
+    for (const key in student_info){
+        console.log(key + ": " + student_info[key]);
+    }
+
+
